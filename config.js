ecde1cf0541076ebbbde270f2c843f183edd2cea
AnhLee404 <94614869+AnhLee404@users.noreply.github.com>
2021-11-18 19:36:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    titleWeb: "Chrion1712",
+    introTitle: 'Hey bae!',
+    introDesc: `Hôm nay anh học toán hình\n
+    Tròn vuông chẳng có,toàn hình bóng emm�^^..`,
+    btnIntro: '😳',
+    title: 'Cậu có thích tui hơmm 🥰',
+    desc: 'Hong trả lời tui tính là thích , thoát ra tui cũng tính là thích luôn nhaaa☺️
+ ',
+    btnYes: 'Cóooo😚
+',
+    btnNo: 'Không nha😏',
+    question: 'Orhhh cậu thích tui ở điểm nào áaa,inbox nói tui nghe luôn i😝',
+    btnReply: 'Gửi cho AnhLee😇',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/AnhLee404/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
